57a073898a91f0772f00ca52d9ae7b656ac97221
whatbe <whatbe@home.be>
2021-01-12 14:06:27 +0100

    update

--- prime/gen.c
+++ prime/gen.c
@@ -5,29 +5,27 @@
 
 #define LIM 1e10
 
-typedef uint64_t u64;
-
 
 int main()
 {
-	char *p = malloc(LIM);
+	uint8_t *p = malloc(LIM);
 	if (!p) exit(1);
 	memset(p, 1, 2);
 
-	u64 o;
+	uint64_t o;
 
-	for (u64 n = 4; n < LIM; n+=2) p[n] = 1;
-	for (u64 n = 3; n*n < LIM; n+=2)
+	for (uint64_t n = 4; n < LIM; n+=2) p[n] = 1;
+	for (uint64_t n = 3; n*n < LIM; n+=2)
 	{
 		if (!p[n])
 		{
 			o = n << 1;
-			for (u64 m = n*n; m < LIM; m+=o) p[m] = 1;
+			for (uint64_t m = n*n; m < LIM; m+=o) p[m] = 1;
 		}
 	}
 
 	/*
-	for (u64 i = 0; i < LIM; i++)
+	for (uint64_t i = 0; i < LIM; i++)
 		if (!p[i])
 			printf("%lu\n", i);
 			*/
